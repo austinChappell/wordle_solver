e74fa348e9bcffdb1d60255e77d72976f8bcb0b3
Austin Chappell <austin.w.chappell@gmail.com>
2022-01-29 21:54:40 -0600

    Filter remaining words

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [words, setWords] = useState<string[] | null>(null);
+  const [attemptedWords, setAttemptedWords] = useState<string[]>([]);
+  const [excludedLetters, setExcludedLetters] = useState<string[]>([]);
+  const [confirmedLetters, setConfirmedLetters] = useState<(string | null)[]>([
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+
+  const remainingPossibleWords = useMemo(() => {
+    const remainingWords = (words ?? [])
+      .filter(w => !attemptedWords.includes(w))
+      .filter(w => excludedLetters.some(letter => w.includes(letter)));
+
+    if (!confirmedLetters.filter(Boolean).length) {
+      return remainingWords;
+    }
+
+    return remainingWords
+      .filter(word =>
+        confirmedLetters
+          .every((letter, index) =>
+            letter === null || word[index] === letter))
+  }, [attemptedWords, confirmedLetters, excludedLetters, words]);
+
+  useEffect(() => {
+    console.log('fetching words....')
+
+    fetch('./words.txt')
+      .then((r) => r.text())
+      .then(text => {
+        setWords(text.split('\n').filter(word => word.length === 5));
+      })
+  }, []);
+
+  console.log('words : ', words);
+
   return (
     <div className="App">
       <header className="App-header">
